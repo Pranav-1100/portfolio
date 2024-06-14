20bfefe3171d3fd766023543592b27ae6b0a2fa4
Pranav <pranavaggarwal1100@gmail.com>
2024-06-14 11:39:32 +0530

    updated projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-
+import weather from "../../Assets/Screenshot 2024-06-14 113610.png";
+import quiz from "../../Assets/image.png";
 
 function Projects() {
   return (
@@ -18,12 +19,23 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              
+              imgPath={weather}
               isBlog={false}
               title="Weather-App"
               description="Your go-to source for real-time weather updates by searching your city."
               ghLink="https://github.com/Pranav-1100/weather-app"
               demoLink="https://custom-weather-react.vercel.app"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={quiz}
+              isBlog={false}
+              title="Quiz-App"
+              description="Just a Small quiz"
+              ghLink="https://github.com/Pranav-1100/Quiz"
+              demoLink="https://quiz-github123.vercel.app/"
             />
           </Col>
 
